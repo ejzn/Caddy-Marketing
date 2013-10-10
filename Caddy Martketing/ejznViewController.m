3222c08b295cc3d3c5ba8b15c6b2dc69b80196ab
Erik Johnson <erikj54@gmail.com>
2013-10-10 13:05:02 -0700

    Initial Commit

---
+++ Caddy Martketing/ejznViewController.m
@@ -0,0 +1,29 @@
+//
+//  ejznViewController.m
+//  Caddy Martketing
+//
+//  Created by Erik Johnson on 2013-10-10.
+//  Copyright (c) 2013 Erik Johnson. All rights reserved.
+//
+
+#import "ejznViewController.h"
+
+@interface ejznViewController ()
+
+@end
+
+@implementation ejznViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Caddy Martketing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Caddy Martketing
+//
+//  Created by Erik Johnson on 2013-10-10.
+//  Copyright (c) 2013 Erik Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ejznAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ejznAppDelegate class]));
+    }
+}
